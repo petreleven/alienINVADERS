13208738325f92339b6dff05e8e96f3a81dfcf29
petreleven <93506710+petreleven@users.noreply.github.com>
2021-11-01 08:33:04 +0300

    contains the alien's behaviours

---
+++ alien.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+    def __init__(self,ai_settings,screen):
+        super(Alien,self).__init__()
+        self.screen=screen
+        self.ai_setting=ai_settings
+
+        self.image=pygame.image.load('C:\\Users\\peter\\Desktop\\alieninvasion\\images\\untitled .bmp')
+        self.rect=self.image.get_rect()
+        
+        
+        self.rect.x=self.rect.width
+        self.rect.y=self.rect.height
+
+        self.x=float(self.rect.x)
+
+    def check_edges(self):
+        screen_rect=self.screen.get_rect()
+        if self.rect.right>=screen_rect.right:
+            return True
+        elif self.rect.left<=0:
+            return True
+    
+
+    def update(self):
+        self.x+=(self.ai_setting.alienSpeedFactor*self.ai_setting.fleet_direction)
+        self.rect.x=self.x
+
+    
+    def blitme(self):
+        self.screen.blit(self.image,self.rect)
